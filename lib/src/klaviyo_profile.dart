a570338a7be84ad11d4c9be6742f52cff9a681bf
Artem Medvediev <medved196922@gmail.com>
2023-06-26 22:00:33 +0300

    added missing fields, refactored parsing on native

--- lib/src/klaviyo_profile.dart
+++ lib/src/klaviyo_profile.dart
@@ -6,11 +6,15 @@ class KlaviyoProfile extends Equatable {
   final String? phoneNumber;
   final String? firstName;
   final String? lastName;
+  final String? organization;
+  final String? title;
+  final String? image;
   final String? address1;
   final String? address2;
   final String? region;
   final String? latitude;
   final String? longitude;
+  final Map<String, dynamic>? properties;
 
   const KlaviyoProfile({
     this.id,
@@ -18,20 +22,43 @@ class KlaviyoProfile extends Equatable {
     this.phoneNumber,
     this.firstName,
     this.lastName,
+    this.organization,
+    this.title,
+    this.image,
     this.address1,
     this.address2,
     this.region,
     this.latitude,
     this.longitude,
+    this.properties,
   });
 
   @override
-  List<Object?> get props =>
-      [id, email, phoneNumber, lastName, address1, region, latitude, longitude];
+  List<Object?> get props => [
+        id,
+        email,
+        phoneNumber,
+        firstName,
+        lastName,
+        organization,
+        title,
+        image,
+        address1,
+        address2,
+        region,
+        latitude,
+        longitude,
+        properties,
+      ];
 
   @override
-  String toString() =>
-      'ProfileModel(id: $id, email: $email, phoneNumber: $phoneNumber, lastName: $lastName, address1: $address1, region: $region, latitude: $latitude, longitude: $longitude)';
+  String toString() {
+    var properties = toJson().toString();
+
+    var propsWithoutBrackets = properties.substring(1, properties.length - 1);
+
+    return 'KlaviyoProfile($propsWithoutBrackets)';
+  }
 
   Map<String, dynamic> toJson() => {
         'external_id': id,
@@ -39,10 +66,14 @@ class KlaviyoProfile extends Equatable {
         'phone_number': phoneNumber,
         'first_name': firstName,
         'last_name': lastName,
+        'organization': organization,
+        'title': title,
+        'image': image,
         'address1': address1,
         'address2': address2,
         'region': region,
         'latitude': latitude,
         'longitude': longitude,
+        'properties': properties,
       };
 }
